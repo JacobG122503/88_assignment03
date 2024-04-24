d156e7d8069bebdad0fbdebcb185ab83e0f36482
Kate Endersby <kateende@iastate.edu>
2024-04-24 17:56:17 -0500

    create form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,130 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const [viewer, setViewer] = useState(0);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {};
 
   function Create() {
-    return (<div>Create</div>);
+    return (
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="container mt-5"
+        id="paymentForm"
+      >
+        <div className="form-group">
+          <p>ID</p>
+          <input
+            {...register("id", { required: true })}
+            placeholder=""
+            className="form-control"
+            id="id"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Title</p>
+          <input
+            {...register("name", { required: true })}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Price</p>
+          <input
+            {...register("price", {
+              required: true,
+              minLength: 16,
+              maxLength: 16,
+            })}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Description</p>
+          <input
+            {...register("description", { required: true })}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Image URL</p>
+          <input
+            {...register("category")}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Category</p>
+          <input
+            {...register("category")}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <p>Rating</p>
+          <input
+            {...register("rating", { required: true })}
+            placeholder=""
+            className="form-control"
+          />
+        </div>
+
+        <br />
+        <button type="submit" className="btn btn-secondary">
+          Create Product
+        </button>
+        <br />
+      </form>
+    );
   }
 
   function Read() {
     fetch("http://localhost:8081/listProducts")
-    .then((response) => response.json())
-    .then((products) => {
-      loadProducts(products);
-    });
+      .then((response) => response.json())
+      .then((products) => {
+        loadProducts(products);
+      });
 
-  function loadProducts(products) {
-    // Find the element “col” in HTML
-    var productCard = document.createElement("div");
+    function loadProducts(products) {
+      // Find the element “col” in HTML
+      var productCard = document.createElement("div");
 
-    
-    for (var i = 1; i <= products.length; i++) {
-      let item = products.find((product) => product.id === i);
-      if (item) {
-        let card = "card" + i.toString();
-        let id = item.id;
-        let name = item.title;
-        let price = item.price;
-        let url = item.imageUrl;
-        let description = item.description;
-        let cat = item.category;
-        let rating = item.rating;
+      for (var i = 1; i <= products.length; i++) {
+        let item = products.find((product) => product.id === i);
+        if (item) {
+          let card = "card" + i.toString();
+          let id = item.id;
+          let name = item.title;
+          let price = item.price;
+          let url = item.imageUrl;
+          let description = item.description;
+          let cat = item.category;
+          let rating = item.rating;
 
-        // create a new HTML div division
-        let AddCard = document.createElement("div");
-        // add class = “col” to new division for Bootstrap
-        AddCard.classList.add("col");
-        // create Bootstrap card
-        AddCard.innerHTML = `
+          // create a new HTML div division
+          let AddCard = document.createElement("div");
+          // add class = “col” to new division for Bootstrap
+          AddCard.classList.add("col");
+          // create Bootstrap card
+          AddCard.innerHTML = `
         <div id=${card} class="card shadow-sm">
             <img src=${url} class="card-img-top" alt="..."></img>    
             <div  class="card-body">
@@ -53,50 +138,80 @@ function App() {
                 </div>
             </div>
         </div> `;
-        // append new division
-        productCard.appendChild(AddCard);
-      }
-    } // end of for
-    return (productCard);
-   
+          // append new division
+          productCard.appendChild(AddCard);
+        }
+      } // end of for
+      return productCard;
+    }
   }
-}
 
+  function Update() {
+    return <div>Update</div>;
+  }
 
-function Update() {
-  return (<div>Update</div>);
-}
+  function Delete() {
+    return <div>Delete</div>;
+  }
 
-function Delete() {
-  return(<div>Delete</div>);
-}
+  const viewCreate = () => {
+    setViewer(0);
+  };
 
-const viewCreate = () => {
-  setViewer(0);
-}
+  const viewRead = () => {
+    setViewer(1);
+  };
 
-const viewRead = () => {
-  setViewer(1);
-}
+  const viewUpdate = () => {
+    setViewer(2);
+  };
 
-const viewUpdate = () => {
-  setViewer(2);
-}
+  const viewDelete = () => {
+    setViewer(3);
+  };
 
-const viewDelete = () => {
-  setViewer(3);
-}
-
-  return (<div>
-    <button type="button" className="btn btn-primary" onClick={viewCreate}>Create</button>
-    <button type="button" className="btn btn-primary" onClick={viewRead}>Read</button>
-    <button type="button" className="btn btn-primary" onClick={viewUpdate}>Update</button>
-    <button type="button" className="btn btn-primary" onClick={viewDelete}>Delete</button>
-    {viewer === 0 && <Create />}
-    {viewer === 1 && <Read />}
-    {viewer === 2 && <Update />}
-    {viewer == 3 && <Delete />}
-  </div>)
+  return (
+    <div>
+      <header data-bs-theme="dark">
+        <div class="navbar navbar-dark bg-dark shadow-sm">
+          <div class="container">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={viewCreate}
+            >
+              Create
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={viewRead}
+            >
+              Read
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={viewUpdate}
+            >
+              Update
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={viewDelete}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </header>
+      {viewer === 0 && <Create />}
+      {viewer === 1 && <Read />}
+      {viewer === 2 && <Update />}
+      {viewer == 3 && <Delete />}
+    </div>
+  );
 }
 
 export default App;
